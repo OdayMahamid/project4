77e73cdcb106f80e707fcbd299a13ed7a92fdc74
oday shibli <oday.shibli.99@gmail.com>
2021-09-30 09:44:57 +0300

    add script.groovy

---
+++ script.groovy
@@ -0,0 +1,16 @@
+def clone(){
+git branch: 'main', url: 'https://github.com/spring-guides/gs-gradle'
+}
+def build(){
+    script{
+        dir("complete"){
+            sh ./gradlew clean build
+            sh /gradlew jar
+        }
+    }
+}
+def artifact(){
+  archiveArtifacts artifacts: '**/*.tar', followSymlinks: false
+}
+return this
+
